58af02ad86508b636782c5e04adce29c47a9a32a
Ashes Das <127014640+Ashes2004@users.noreply.github.com>
2025-09-24 14:41:30 +0000

    Update README.md to mark query 2 as completed and add Q2.sql for the corresponding SQL example

---
+++ Q2.sql
@@ -0,0 +1,11 @@
+-- 2. Select only the first_name and last_name from employees.
+
+SELECT concat(first_name , " " , last_name) as Full_Name FROM EMPLOYEES;
+
+--result : 
+-- Full_Name
+-- John Doe
+-- Jane Smith
+-- Mike Johnson
+-- Sarah Williams
+-- David Brown
